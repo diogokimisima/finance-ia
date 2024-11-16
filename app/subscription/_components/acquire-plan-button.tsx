976047b15699f3cf66881cde19b289719ccbfd8d
dkimisima@gmail.com <dkimisima@gmail.com>
2024-11-16 15:49:36 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -3,8 +3,12 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
+
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
 
@@ -23,12 +27,32 @@ const AcquirePlanButton = () => {
     await stripe.redirectToCheckout({ sessionId });
   };
 
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
+  if (!process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL) {
+    throw new Error("Stripe customer portal URL is not defined");
+  }
+
+  if (hasPremiumPlan) {
+    return (
+      <Button
+        className="w-full rounded-full font-bold text-primary"
+        variant="outline"
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL}?prefilled_email=${user?.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       onClick={handleAcquirePlanClick}
       className="w-full rounded-full font-bold"
     >
-      Adquirir plano
+      Adquirir Plano
     </Button>
   );
 };
